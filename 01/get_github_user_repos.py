61ad1a6c9c8a8c06553c5b74d17bbc861c406ca6
Wrexan <fwrexan@gmail.com>
2021-10-08 19:43:46 +0300

    Done for 0101

--- 01/get_github_user_repos.py
+++ 01/get_github_user_repos.py
@@ -1,17 +1,26 @@
 import requests
+import json
 
 name = 'Wrexan'
 my_params = {
     'q': name
 }
-
-url = "https://api.github.com/users/yuwinzer/repos"
+export_data = {'repositories': []}
+user_name = 'yuwinzer'
+url = 'https://api.github.com/users/'+user_name+'/repos'
 
 response = requests.get(url, params=my_params)
-text = response.text
 if response.ok:
     j_data = response.json()
     for i in range(len(j_data)):
-        print(j_data[i].get("name"))
+        repo_name = j_data[i].get("name")
+        export_data['repositories'].append({
+            'name': user_name,
+            'repo': repo_name})
+        print(repo_name)
 else:
     print(response)
+
+
+with open(user_name+'_repos.json', 'w') as outfile:
+    json.dump(export_data, outfile)
